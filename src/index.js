d28ae258957146b6a5c771e908619ffb2af19d1d
Dastan <kaipov020@gmail.com>
2020-09-03 20:07:15 +0600

    todo

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const Hello = () => {
+   <div>
+       <h1>React first App</h1>
+       <p> It is news</p>
+   </div>
+}
 ReactDOM.render(
- <h1>Hello</h1>,
+<Hello/>,
   document.getElementById('root')
 );
