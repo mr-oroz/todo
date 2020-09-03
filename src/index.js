f98ace8ec0f11b52f5b0217afd63c98a2acd9348
Dastan <kaipov020@gmail.com>
2020-09-03 21:02:17 +0600

    display

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import ReactDOM from 'react-dom';
 const Blog = (props) => {
     return (
         <div>
-            <div className="card" style={{width: '200px', display: "flex"}}>
+            <div className="card" style={{width: '200px',display: "flex", fontSize: "20px" }}>
                 <img className="card-img-top"
                      src="https://i.pinimg.com/originals/57/66/74/576674777c929c18f612c451ab934707.jpg"
                      alt="Card image cap"/>
                 <div className="card-body">
-                    <h5 className="card-title"></h5>
+                    <h5 className="card-title">{props.title}</h5>
                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of
                         the card's content.</p>
                 </div>
@@ -19,11 +19,11 @@ const Blog = (props) => {
 }
 const Projects = () => {
     return (
-        <div>
-            <Blog title='text'/>
-            <Blog/>
-            <Blog/>
-            <Blog/>
+        <div style={{display:"inline-flex"}}>
+            <Blog title='Tort'/>
+            <Blog title="Pirog"/>
+            <Blog title="Marmiald"/>
+            <Blog title="Yogurt"/>
         </div>
 )
 }
