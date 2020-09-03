4e666e8d7e8e9b4312131f0d8febf2f15f44be07
Dastan <kaipov020@gmail.com>
2020-09-03 20:55:44 +0600

    tort

--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Hello = () => {
-   <div>
-       <h1>React first App</h1>
-       <p> It is news</p>
-   </div>
+const Blog = (props) => {
+    return (
+        <div>
+            <div className="card" style={{width: '200px', display: "flex"}}>
+                <img className="card-img-top"
+                     src="https://i.pinimg.com/originals/57/66/74/576674777c929c18f612c451ab934707.jpg"
+                     alt="Card image cap"/>
+                <div className="card-body">
+                    <h5 className="card-title"></h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of
+                        the card's content.</p>
+                </div>
+            </div>
+        </div>
+)
+}
+const Projects = () => {
+    return (
+        <div>
+            <Blog title='text'/>
+            <Blog/>
+            <Blog/>
+            <Blog/>
+        </div>
+)
 }
 ReactDOM.render(
-<Hello/>,
+<Projects/>,
   document.getElementById('root')
 );
